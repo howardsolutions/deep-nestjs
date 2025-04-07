f6e652707ba2e03373b6d4833d59dff2907858f2
howardsolutions <phungthinh0946519356@gmail.com>
2025-04-07 12:13:29 -0600

    feat: api to signup a user

--- car-value/src/users/users.controller.ts
+++ car-value/src/users/users.controller.ts
@@ -1,10 +1,13 @@
 import { Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
+    constructor(private userService: UsersService) { }
+
     @Post("/signup")
     createUser(body: CreateUserDto) {
-        console.log(body)
+        return this.userService.create(body.email, body.password);
     }
 }
--- car-value/src/users/users.service.ts
+++ car-value/src/users/users.service.ts
@@ -1,6 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './users.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class UsersService {
-    
+    constructor(@InjectRepository(User) private repository: Repository<User>) { }
+
+    create(email: string, password: string) {
+        const user = this.repository.create({email, password});
+
+        return this.repository.save(user);
+    }
 }
