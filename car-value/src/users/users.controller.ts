ade24c31aafff46e1751006a98ba3b95b426ba0d
howardsolutions <phungthinh0946519356@gmail.com>
2025-04-08 09:59:34 -0600

    feat: update dependencies and enhance user serialization with class-transformer

--- car-value/src/users/users.controller.ts
+++ car-value/src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query } from '@nestjs/common';
+import { Body, ClassSerializerInterceptor, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query, UseInterceptors } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
@@ -12,6 +12,7 @@ export class UsersController {
         return this.userService.create(body.email, body.password);
     }
 
+    @UseInterceptors(ClassSerializerInterceptor)
     @Get("/:id")
     findUser(@Param('id') id: string) {
         return this.userService.findOne(parseInt(id));
