2455256c0640f48f7c10c91f98db3c223e7cb718
howardsolutions <phungthinh0946519356@gmail.com>
2025-04-09 14:41:47 -0600

    chore: setup cookies

--- car-value/src/main.ts
+++ car-value/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import cookieSession from 'cookie-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // for Cookies
+  app.use(cookieSession(
+    {
+      keys: ["abcd"]
+    }
+  ))
+
+  // Validation
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true
